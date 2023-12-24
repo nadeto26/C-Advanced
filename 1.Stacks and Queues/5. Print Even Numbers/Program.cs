48e74b5ed94922878f107ea213e46ab65c27fbbb
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-24 13:00:19 +0200

    Test

---
+++ 1.Stacks and Queues/5. Print Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Queue<int> stack = new Queue<int>();
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] % 2 == 0)
+    {
+        stack.Enqueue(nums[i]);
+
+
+    }
+
+}
+
+Console.WriteLine(string.Join(", ", stack));
