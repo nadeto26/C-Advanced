d0f6a201f3be2db08b6c7e0913e5b33d543b3426
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-24 13:00:40 +0200

    Test

---
+++ 1.Stacks and Queues/6. Supermarket/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> queue = new Queue<string>();
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "End") // прекратяваме цикъла и отпечваме броя на останалите клиенти 
+                {
+                    Console.WriteLine($"{string.Join(" ", queue.Count)} people remaining.");
+                    break;
+
+                }
+                else if (name == "Paid") // принтираме имената на клиентите от опашката , след това трябва да изпразним опашката!!!!
+                {
+                    foreach (string namez in queue)
+                    {
+                        Console.WriteLine(string.Join(" ", namez));
+                    }
+
+
+                    for (int i = 0 - 1; i <= queue.Count; i++)
+                    {
+
+                        queue.Dequeue();
+                    }
+
+
+
+                }
+                else
+                {
+                    queue.Enqueue(name);
+                }
+            }
+    }
+}
