02ee31aacd1a05da3432f36d576fd27c65ddbe30
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-25 10:24:49 +0200

    Test

---
+++ 02.Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>(input);
+            Queue<char> queue = new Queue<char>(input);
+
+            while (stack.Count > 0)
+            {
+                char firstStack = stack.Peek();
+                char firstQueue = queue.Peek();
+
+                if (firstStack == '{' && firstQueue == '}')
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                    Console.WriteLine("Yes");
+                    continue;
+
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    break;
+                }
+
+                if (firstStack == '[' && firstQueue == ']')
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                    Console.WriteLine("Yes");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    break;
+                }
+                if (firstStack == '(' && firstQueue == ')')
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                    Console.WriteLine("Yes");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    Console.WriteLine("Yes");
+                    break;
+                }
+            }
+        }
+    }
+}
