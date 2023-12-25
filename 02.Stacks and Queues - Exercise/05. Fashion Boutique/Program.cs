a5c2ccd15fae022b71c839f33e4c0892247b86c4
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-25 10:21:43 +0200

    Test

---
+++ 02.Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var priceOfClothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int capacityOfARack = int.Parse(Console.ReadLine());
+
+            Stack<int> prices = new Stack<int>(priceOfClothes);
+
+            int sumOfClothes = 0;
+            int numOfRacks = 1;
+
+            //5 4 8 6 3 8 7 7 9 --- orders
+            //16 -- capacity
+
+            for (int i = priceOfClothes.Length - 1; i >= 0; i--)
+            {
+                sumOfClothes += priceOfClothes[i];
+
+                if (sumOfClothes <= capacityOfARack)
+                {
+                    if (prices.Any())
+                    {
+                        prices.Pop();
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    i++;
+                    numOfRacks++;
+                    sumOfClothes = 0;
+                }
+            }
+
+            Console.WriteLine(numOfRacks);
+        }
+    }
+    }
