b7d2109c5c2a5596796461f2f6c6a22184459fcc
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-26 13:44:42 +0200

    Test

---
+++ Нова папка/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string sumbols = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = sumbols[col];
+                }
+            }
+
+            char countainSumbol = char.Parse(Console.ReadLine());
+
+            bool isFound = false;
+
+            for (int row = 0; row < size; row++)
+            {
+
+
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] == countainSumbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        return;
+                    }
+
+                }
+            }
+
+            if (isFound == false)
+            {
+                Console.WriteLine($"{countainSumbol} does not occur in the matrix ");
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
