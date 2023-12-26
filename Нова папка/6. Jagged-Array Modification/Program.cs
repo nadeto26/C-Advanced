c2493202e3a754a56085457753b38acd464a9555
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-26 13:45:32 +0200

    Test

---
+++ Нова папка/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            nt size = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[size][];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+                matrix[row] = numbers;
+
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] splittedCommand = command.Split(" ");
+
+                int row = int.Parse(splittedCommand[1]);
+                int cow = int.Parse(splittedCommand[2]);
+                int value = int.Parse(splittedCommand[3]);
+
+                if (row < 0 || row >= matrix.Length || cow < 0 || cow >= matrix.Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+
+                    if (splittedCommand[0] == "Subtract")
+                    {
+                        matrix[row][cow] -= value;
+                    }
+                    else
+                    {
+                        matrix[row][cow] += value;
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+            for (int rol = 0; rol < matrix.Length; rol++)
+            {
+
+                for (int col = 0; col < matrix.Length; col++)
+                {
+
+                    Console.Write(matrix[rol][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
