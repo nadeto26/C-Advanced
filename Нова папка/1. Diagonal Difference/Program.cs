b57c88ecdba8e7d1860d1fd8dc5ebff8ee06ba0d
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-28 10:16:18 +0200

    Test

---
+++ Нова папка/1. Diagonal Difference/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+
+
+            int[,] matrix = new int[rows, rows];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int cow = 0; cow < rows; cow++)
+                {
+                    matrix[row, cow] = numbers[cow];
+                }
+            }
+
+            int primery = 0;
+            int secendary = 0;
+
+            for (int i = 0; i < rows; i++) //  заедно се ппоменят 
+            {
+                primery += matrix[i, i];
+                secendary += matrix[rows - i - 1, i];
+            }
+
+            Console.WriteLine(Math.Abs(primery - secendary));
+
+
+
+        }
+    }
+}
