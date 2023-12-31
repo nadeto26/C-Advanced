420deed38608dfd2c7830476a7f10466621eedae
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-31 08:48:14 +0200

    Test

---
+++ Нова папка/07. Parking Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string input = Console.ReadLine();
+
+
+            while (input != "END")
+            {
+                string[] splittedInput = input.Split(", ");
+
+                string command = splittedInput[0];// in or out 
+
+                string registration = splittedInput[1];
+
+                if (command == "IN")
+                {
+                    cars.Add(registration);
+                }
+                else
+                {
+                    cars.Remove(registration);
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+
+        }
+    }
+}
