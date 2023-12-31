0f97d4048e185a4182cf0fa5b732e920880e0401
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-31 08:46:34 +0200

    Test

---
+++ Нова папка/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(" ").Select(int.Parse).OrderByDescending(n => n)
+ .ToArray();
+            int count = integers.Length >= 3 ? 3 : integers.Length;
+            for (int i = 0; i < count; i++)
+                Console.Write($"{integers[i]} ");
+        }
+    }
+}
