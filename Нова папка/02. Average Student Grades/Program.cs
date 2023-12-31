1d280d802baf4d6e5466833d177656b2afa65c80
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-31 08:46:07 +0200

    Test

---
+++ Нова папка/02. Average Student Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new Dictionary<string, List<decimal>>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var grade = decimal.Parse(tokens[1]);
+                if (!grades.ContainsKey(name))
+                    grades[name] = new List<decimal>();
+                grades[name].Add(grade);
+            }
+            foreach (var (name, studentsGrades) in grades)
+            {
+                var average = studentGrades.Average();
+                Console.Write($"{name} -> ");
+                foreach (var grade in studentGrades)
+                    Console.Write($"{grade:f2} ");
+                Console.WriteLine($"(avg: {average:f2})");
+            }
+
+        }
+    }
+}
