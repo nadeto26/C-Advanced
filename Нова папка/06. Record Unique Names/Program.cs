718b2ac5305076ca52d1823b00e182912cbf3cb2
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-12-31 08:47:49 +0200

    Test

---
+++ Нова папка/06. Record Unique Names/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> list = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                list.Add(name);
+
+            }
+
+
+            foreach (var nam in list)
+            {
+                Console.WriteLine(nam);
+            }
+
+        }
+    }
+}
