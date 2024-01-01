85bf18bcf16bb5c99ba37c4437716784fa9b4fd9
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-01 09:47:41 +0200

    Test

---
+++ Нова папка/01. Unique Usernames/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> list = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                list.Add(name);
+            }
+
+            foreach (string name in list)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
