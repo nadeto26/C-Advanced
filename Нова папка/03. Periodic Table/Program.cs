01fee572ad469434fdf5a1f02bd96da55ec83b78
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-01 09:48:36 +0200

    Test

---
+++ Нова папка/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> result = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] splittedCommand = Console.ReadLine().Split(" ");
+
+                foreach (var command in splittedCommand)
+                {
+                    result.Add((command));
+                }
+            }
+
+            foreach (var re in result)
+            {
+                Console.Write(re + " ");
+            }
+        }
+    }
+}
