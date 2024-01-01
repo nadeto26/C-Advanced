75f6c191ff73d247d87e0c0320bb0ad260fc4b46
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-01 09:49:29 +0200

    Test

---
+++ Нова папка/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            SortedDictionary<char, int> charCounts = new();
+
+            foreach (var ch in input)
+            {
+                if (!charCounts.ContainsKey(ch))
+                {
+                    charCounts.Add(ch, 0);
+                }
+
+                charCounts[ch]++;
+            }
+
+            foreach (var ch in charCounts)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s ");
+            }
+
+        }
+    }
+}
