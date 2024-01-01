bf109adf283eaa12ffc6ad646148b3c9b4e8b8c1
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-01 09:49:02 +0200

    Test

---
+++ Нова папка/04. Even Times/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (result.ContainsKey(num))
+                {
+                    result[num]++;
+                }
+                else
+                {
+                    result.Add(num, 1);
+                }
+
+            }
+            var evenTimesNumber = result.First(kvp => kvp.Value % 2 == 0).Key;
+
+            Console.WriteLine(evenTimesNumber);
+
+
+        }
+    }
+}
