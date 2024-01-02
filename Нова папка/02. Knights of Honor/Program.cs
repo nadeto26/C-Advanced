dd8b504a18030dba8a864e014cf1666a42cea07d
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:39:03 +0200

    Test

---
+++ Нова папка/02. Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            Action<string> addPrefix = name => Console.WriteLine("Sir " + name);
+
+            foreach (var name in names)
+            {
+                addPrefix(name);
+            }
+        }
+    }
+}
