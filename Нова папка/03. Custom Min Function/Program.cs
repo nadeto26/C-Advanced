897e4bd1d902e5482ce778e06bb862f9a5e5bf6e
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:39:50 +0200

    Test

---
+++ Нова папка/03. Custom Min Function/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            Func<List<int>, int> addPrefix = numbers => numbers.Min();
+
+            Console.WriteLine(addPrefix(numbers));
+        }
+    }
+}
