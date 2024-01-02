f7cd366c07c26f4d6536a0bad9943217fbadd706
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:38:34 +0200

    Test

---
+++ Нова папка/01. Action Print/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split(" ").ToList();
+
+            Action<string> print = name => Console.WriteLine(name);
+
+            list.ForEach(print);
+        }
+    }
+}
