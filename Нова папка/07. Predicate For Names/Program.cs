b3e83abc07371b1a0b72beabcd70d87d59320f92
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:41:01 +0200

    Test

---
+++ Нова папка/07. Predicate For Names/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            List<string> list = new List<string>();
+
+            foreach (string name in names)
+            {
+                Predicate<string> predicate = name => name.Length <= n;
+
+                if (!predicate(name))
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(name);
+                }
+            }
+
+            foreach (string li in list)
+
+            {
+                Console.WriteLine(li);
+            }
+        }
+    }
+}
