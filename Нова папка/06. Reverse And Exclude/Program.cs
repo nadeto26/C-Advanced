bd0c253ab866a326aa7de36ab56714e7446fffd8
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:40:39 +0200

    Test

---
+++ Нова папка/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int delitel = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            foreach (int num in numbers)
+            {
+                Predicate<int> predicate = num => num % delitel == 0;
+
+                if (!predicate(num))
+                {
+                    list.Add(num);
+                }
+            }
+            list.Reverse();
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
