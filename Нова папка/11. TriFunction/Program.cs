5664d30fc6bfe4c01f369dee4ff721b650f358e1
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-02 11:42:50 +0200

    Test

---
+++ Нова папка/11. TriFunction/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            Console.WriteLine(names.First(name => name.Select(symbol => (int)symbol).Sum() >= n));
+        }
+    }
+}
