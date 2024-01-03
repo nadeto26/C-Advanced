9939866305de87e3b8ea03557e717ad4987d44cc
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-03 18:55:27 +0200

    Test

---
+++ dd/02. Sum Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
