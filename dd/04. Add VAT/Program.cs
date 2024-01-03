812efd9c891d413d76afd81baf320a073f856141
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-03 18:56:05 +0200

    Test

---
+++ dd/04. Add VAT/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ").Select(n => decimal.Parse(n)).Select(n => n * 1.2m).ToArray();
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine($"{num:F2}");
+            }
+        }
+    }
+}
