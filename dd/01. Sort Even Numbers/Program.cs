64dc1c5910141ee66bf8712afa7884fc45bb6dfe
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-03 18:55:07 +0200

    Test

---
+++ dd/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(n => int.Parse(n)).Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
