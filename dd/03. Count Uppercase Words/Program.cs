f51c1e6cc6012eeb113d75a716ed9d7b0ecc869e
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-03 18:55:46 +0200

    Test

---
+++ dd/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = n => n[0] == n.ToUpper()[0];
+
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(input => predicate(input)).ToArray();
+
+            foreach (var inputItem in input)
+            {
+                Console.WriteLine(inputItem);
+            }
+        }
+    }
+}
