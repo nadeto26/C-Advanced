5dbcb79d14ff1e308e32e68a128bd6f704b3b72a
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-04 17:15:04 +0200

    Test

---
+++ Нова папка/03. Zoo/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+
+		// полета и пропъртис 
+
+		private string species;
+
+		public string Species
+		{
+			get { return species; }
+			set { species = value; }
+		}
+
+		private string diet;
+
+		public string Diet
+		{
+			get { return diet; }
+			set { diet = value; }
+		}
+
+		private double weight;
+
+		public double Weight
+		{
+			get { return  weight; }
+			set { weight = value; }
+		}
+
+		private double length;
+
+		public double Length
+		{
+			get { return length; }
+			set { length = value; }
+		}
+
+		// конструктори 
+
+		public Animal (string spesies , string diet , double weight, double length)
+		{
+			this.Species = spesies;
+			this.Diet = diet;
+			this.Weight = weight;
+			this.Length = length;
+		}
+
+        // ToString
+
+        public override string ToString()
+        {
+
+			StringBuilder st = new StringBuilder();
+
+			st.Append($"The {species} is a {diet} and weighs {weight} kg.");
+
+            return st.ToString().Trim();
+        }
+
+
+    }
+}
---
+++ Нова папка/03. Zoo/Zoo.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+
+        //полетата и пропъртис 
+        private List<Animal> animals;
+
+        public List<Animal> Animals
+        {
+            get { return animals; }
+            set { animals = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int capacity;
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        // конструктор 
+
+        public Zoo(string name,int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            animals = new List<Animal>();
+        }
+
+        //методите 
+
+        public string AddAnimal(Animal animal)
+        {
+            if (animal == null || animals.Count<0)
+            {
+                return "Invalid animal species.";
+            }
+            else if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            else if (animals.Count>Capacity)
+            {
+                return "The zoo is full.";
+            }
+
+            animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        
+            =>animals.RemoveAll(a => a.Species == species);
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+
+            => animals.Where(a => a.Diet == diet).ToList();
+
+
+        public Animal GetAnimalByWeight(double weight)
+
+          => animals.FirstOrDefault(s => s.Weight == weight);
+
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            IEnumerable<Animal> animalList = 
+                animals.Where(s => s.Length >=minimumLength && s.Length<=maximumLength).ToList();
+            StringBuilder st = new StringBuilder();
+
+            if (animalList.Any())
+            {
+                st.AppendLine($"There are {animalList.Count()} animals with a length between" +
+                    $" {minimumLength} and {maximumLength} meters.");
+            }
+            
+            
+
+                
+
+            
+            return st.ToString().Trim();
+
+
+
+        }
+
+
+
+
+    }
+}
