873fe2551f1cc26225051bbc7e4cb617dfb26c03
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-04 17:13:33 +0200

    Test

---
+++ Нова папка/03. Fishing Net/Catalog.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+		private string name;
+
+		public string Name 
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int neededRenovators;
+
+		public int NeededRenovators
+		{
+			get { return neededRenovators; }
+			set { neededRenovators = value; }
+		}
+
+		private string project;
+
+		public string Project
+		{
+			get { return project; }
+			set { project = value; }
+		}
+
+		private List<Renovator> renovators;
+
+		public List<Renovator> Renovators  
+		{
+			get { return renovators; }
+			set { renovators = value; }
+		}
+		public Catalog (string name , int neededRenovators,string project)
+		{
+			Name = name;
+			Project = project;
+			NeededRenovators = neededRenovators;
+			renovators = new List<Renovator>();
+		}
+
+
+		public int Count => renovators.Count;
+
+        public string AddRenovator(Renovator renovator)
+		{
+			if (renovator.Name == null || renovator.Name == string.Empty && renovator.Type == null || renovator.Type == string.Empty)
+			{
+				return "Invalid renovator's information.";
+
+            }
+			else if  (this.Count == this.NeededRenovators)
+			{
+				return "Renovators are no more needed.";
+
+            }
+			else if (renovator.Rate >  350)
+			{
+				return "Invalid renovator's rate.";
+
+            }
+			renovators.Add(renovator);
+			return $"Successfully added {renovator.Name} to the catalog.";
+
+        }
+
+		public bool RemoveRenovator(string name)
+		{
+			var remove = renovators.FirstOrDefault(x => x.Name == name);
+
+			if (remove == null)
+			{
+				return false;
+			}
+			return this.renovators.Remove(remove);
+		}
+
+        public int RemoveRenovatorBySpecialty(string type)
+		{
+			int result = 0;
+            while (this.Renovators.FirstOrDefault(x => x.Type == type) != null)
+            {
+                var targetRenovator = this.Renovators.FirstOrDefault(x => x.Type == type);
+                this.renovators.Remove(targetRenovator);
+                result++;
+            }
+            return result;
+
+        }
+
+        public Renovator HireRenovator(string name)
+		{
+            var targetRenovator = this.Renovators.FirstOrDefault(x => x.Name == name);
+			if (targetRenovator == null)
+			{
+				return null;
+			}
+			this.Renovators.FirstOrDefault(x => x.Name == name).Hired = true;
+			return targetRenovator;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+		{
+			List<Renovator> payRenovators = new List<Renovator>();
+
+			foreach( var pay in payRenovators.Where(x=>x.Days>=days))
+			{
+				payRenovators.Add(pay);
+			}
+			return payRenovators;
+		}
+
+		public string Report()
+		{
+            var sb = new StringBuilder();
+            sb.AppendLine($"Renovators available for Project {this.Project}:");
+            foreach (var item in this.Renovators.Where(x => x.Hired == false).Where(x => x.Paid == false))
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+
+}
+
---
+++ Нова папка/03. Fishing Net/Renovator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+		private string name;
+
+		public string Name 
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string type;
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		private double rate;
+
+		public double Rate
+		{
+			get { return rate; }
+			set { rate = value; }
+		}
+
+		private int days;
+
+		public int Days
+		{
+			get { return days; }
+			set { days = value; }
+		}
+
+		private bool hired= false;
+
+		public bool Hired
+		{
+			get { return hired; }
+			set { hired = value; }
+		}
+
+        private bool paid = false;
+
+        public bool Paid
+        {
+            get { return paid; }
+            set { paid = value; }
+        }
+
+
+        public Renovator ( string name, string type,double rate , int days)
+		{
+			Name = name;
+			Type = type;
+			Rate = rate;
+			Days = days;
+		}
+
+        public override string ToString()
+        {
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendLine($"-Renovator: {name}");
+            sb.AppendLine($"--Specialty: {type}");
+            sb.AppendLine($"--Rate per day: {rate} BGN");
+
+
+
+            return sb.ToString().Trim();
+        }
+
+
+
+
+    }
+}
