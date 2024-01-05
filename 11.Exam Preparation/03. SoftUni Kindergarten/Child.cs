7b382faf7dafe882ea7598fe5534eec5dc0af743
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-05 17:45:19 +0200

    Test

---
+++ 11.Exam Preparation/03. SoftUni Kindergarten/Child.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Child
+    {
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		private string parentName;
+
+		public string ParentName
+		{
+			get { return parentName; }
+			set { parentName = value; }
+		}
+
+		private string contactNumber;
+
+		public string ContactNumber
+		{
+			get { return contactNumber; }
+			set { contactNumber = value; }
+		}
+		public Child(string firstName, string lastName,int age, string parentName, string contactNumber)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			ParentName = parentName;
+			ContactNumber = contactNumber;
+		}
+        public override string ToString()
+        {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Child: {firstName} {lastName}, Age: {age}, Contact info: {parentName} - {contactNumber}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+
+    }
+}
---
+++ 11.Exam Preparation/03. SoftUni Kindergarten/Kindergarten.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int capacity;
+
+		public int Capacity
+		{
+			get { return capacity; }
+			set { capacity = value; }
+		}
+
+		private List<Child> registry;
+
+        public List<Child> Registry
+        {
+			get { return registry; }
+			set { registry = value; }
+		}
+
+		public Kindergarten( string name, int capacity)
+		{
+			Name = name;
+			Capacity = capacity;
+			registry = new List<Child>();
+		}
+
+		public bool AddChild(Child child)
+		{
+			if (registry.Count< capacity)
+			{
+				registry.Add(child);
+				return true;
+			}
+			return false;
+		}
+
+		public bool RemoveChild(string childFullName)
+		{
+			var removed = registry.Find(c => childFullName == $"{c.FirstName} {c.LastName}");
+
+			if (removed == null)
+			{
+				return false;
+			}
+			registry.Remove(removed);
+			return true;
+        }
+
+		public int ChildrenCount => registry.Count;
+
+		public Child GetChild(string childFullName)
+		{
+            return  registry.FirstOrDefault(c => childFullName == $"{c.FirstName} {c.LastName}");
+		  
+        }
+
+		public string RegistryReport()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Registered children in {Name}:");
+
+			foreach(Child child in registry.OrderByDescending(x=>x.Age).OrderBy(x=>x.LastName).OrderBy(x=>x.FirstName))
+			{
+				sb.AppendLine(child.ToString());
+			}
+
+			return sb.ToString().TrimEnd();
+		}
+
+
+
+
+
+
+
+    }
+}
